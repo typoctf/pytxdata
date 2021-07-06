2eb93d3884fd4c0e07cb5be3eceb05d04a6b8a90
Catriona McKay <51414661+catrionamckay@users.noreply.github.com>
2021-07-05 21:02:17 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="pytxdata",
+    version="2020.1",
+    summary="The Olson timezone database for Python",
+    author="Sébastien Eustace",
+    author_email="sebastien@eustace.io",
+    license="MIT"
+)
